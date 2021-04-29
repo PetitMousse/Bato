0229dbdc2c93eaa72eee212c19a77cfcc462021f
la-carpe <64590614+la-carpe@users.noreply.github.com>
2021-04-29 12:42:35 +0200

    conversion vers cartesien

---
+++ conversion_x_lat_VF.py
@@ -0,0 +1,18 @@
+import math
+
+x = float(input("Quelle est la position x ? "))
+y = float (input("Quell est la position y ? "))
+
+pi = 3.14
+R = 6371
+
+
+# conversion (lat,long) --> (x,y)
+# carte du monde de peters (à plat)
+
+long = (x*180)/(R*pi*45)
+calcul = y/(R*math.sqrt(2))
+lat = math.asin(calcul)
+
+print(lat)
+print(long)
