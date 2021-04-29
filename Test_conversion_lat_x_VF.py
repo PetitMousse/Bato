62aa8548d1afeba1b0fc41ee40f95bbcf2836e5a
la-carpe <64590614+la-carpe@users.noreply.github.com>
2021-04-29 12:52:11 +0200

    test_conversion

---
+++ Test_conversion_lat_x_VF.py
@@ -0,0 +1,14 @@
+import math
+
+long = float(input("Quelle est la longitdue ? "))
+lat = float (input("Quell est la lattitude ? "))
+
+pi = 3.14
+R = 6371
+
+# conversion (lat,long) --> (x,y)
+# carte du monde de peters (à plat)
+x = (R*pi*long*45)/180
+y = R*math.sqrt(2)*math.sin(lat)
+
+print (x, y)
